cfe330e1b48f75ce9ff296fe3ca9e14b133128fa
Elliottssu <94slgbest@163.com>
2016-10-11 01:46:11 +0800

    路由href点击测试

--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -1,11 +1,11 @@
 import { ModuleWithProviders }  from '@angular/core';
-import { Routes, RouterModule } from '@angular/router'; 
+import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent}  		from './user';
 const appRoutes: Routes = [
   {
-    path: 'login',  
-    component: LoginComponent  
+    path: 'login',
+    component: LoginComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
--- assets/app/user/login/login.component.ts
+++ assets/app/user/login/login.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'login',
-  templateUrl: 'app/user/login/login.component.html' 
+  templateUrl: 'app/user/login/login.component.html'
 })
 
-export class LoginComponent{ 
-	account = '登陆账户测试';
-}
+export class LoginComponent{}
