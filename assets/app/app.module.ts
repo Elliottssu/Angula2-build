c1a4c30d753a81a0e2afc1811bc1b49d024b70ad
Elliottssu <94slgbest@163.com>
2016-10-13 01:40:00 +0800

    HashLocationStrategy 使用hash路由

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule }       from '@angular/core';
 import { BrowserModule  } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { routing } 		  from './app.routing';
 import { AppComponent }   from './app.component';
@@ -16,5 +17,11 @@ import { LoginComponent}  from './user';
     bootstrap:    [
     	AppComponent
 	],
+  providers: [
+      {
+        provide: LocationStrategy,
+        useClass: HashLocationStrategy
+        }//我们刷新页面，不是像服务器请求跟目录，而是请求的是/about或者/contact，但是，服务器不知道该路径，会返回一个404页面回来。
+  ]//hash strategy的策略是基于锚标记的，像/#/home 或者 /#/contact，并且，服务器会理解/标签，后面的不用管，这样就能拿到正确的资源
 })
 export class AppModule {}
--- assets/app/app.routing.ts
+++ assets/app/app.routing.ts
@@ -6,6 +6,15 @@ const appRoutes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: LoginComponent
   }
 ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
